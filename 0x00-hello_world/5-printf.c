060da4d7c528239af4b195ca3871765728e599d7
HafeezOJ <ho.adepoju@gmail.com>
2022-10-13 21:48:48 +0000

    printed with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints using printf
+ *
+ * Return: 0 (succes)
+ */
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
