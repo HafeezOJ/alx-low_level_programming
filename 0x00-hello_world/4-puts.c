21ce421be92a38c0ca77177d5d54db8e4dfc576b
HafeezOJ <ho.adepoju@gmail.com>
2022-10-13 21:31:29 +0000

    removed comment line more than 80 charaters

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-/* a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line. */
+
+/* a C program that prints exactly with puts 
+ * main - Entry point
+ * prints "Programming is like building a multilingual puzzle
+ * returns 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
