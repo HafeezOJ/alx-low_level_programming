175ad3b324adf8fb8f8417458af88f1e3c1cab46
HafeezOJ <ho.adepoju@gmail.com>
2022-10-15 17:40:48 +0000

    base 16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: Prints lower alphabets from a - z
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char base_hex0_9. base_hexa_f;
+
+	for (base_hex = '0'; base_hex <= '9'; base_hex++)
+	{
+		putchar(base_hex);
+	}
+	for (base_hexa_f = 'a'; basehexa_f <= 'f'; base_hex++)
+	{
+		putchar(base_hexa_f);
+	}
+	putchar('\n');
+	return (0);
+}
