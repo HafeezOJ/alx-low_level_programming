2a0ea6215cd231701b77150831029f17f2352e66
HafeezOJ <ho.adepoju@gmail.com>
2022-10-15 14:19:04 +0000

    modulus after initialization of n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,9 +13,9 @@ int main(void)
 {
 	int n, r;
 
-	r = n % 10;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	r = n % 10;
 	if (r > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, r);
