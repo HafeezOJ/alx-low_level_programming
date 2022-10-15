b8ea479c7fb5c61ec6b84c7639fd6fec851e409e
HafeezOJ <ho.adepoju@gmail.com>
2022-10-15 14:41:10 +0000

    lower and upper char

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: Prints lower alphabets from a - z and upper alphabets A - Z.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lower_char;
+	char upper_char;
+
+	for (lower_char = 'a'; lower_char <= 'z'; lower_char++)
+	{
+		putchar(lower_char);
+	}
+	for (upper_char = 'A'; upper_char <= 'Z'; upper_char++)
+	{
+		putchar(upper_char);
+	}
+	putchar('\n');
+	return (0);
+}
