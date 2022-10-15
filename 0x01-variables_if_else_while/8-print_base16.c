0e3f4b10413b4cbe157d14247fb9004a36b9e760
HafeezOJ <ho.adepoju@gmail.com>
2022-10-15 17:46:33 +0000

    fix all variable typos

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,11 +11,11 @@ int main(void)
 {
 	char base_hex0_9, base_hexa_f;
 
-	for (base_hex0_9 = '0'; base_hex <= '9'; base_hex++)
+	for (base_hex0_9 = '0'; base_hex0_9 <= '9'; base_hex0_9++)
 	{
-		putchar(base_hex);
+		putchar(base_hex0_9);
 	}
-	for (base_hexa_f = 'a'; basehexa_f <= 'f'; base_hex++)
+	for (base_hexa_f = 'a'; basehexa_f <= 'f'; base_hexa_f++)
 	{
 		putchar(base_hexa_f);
 	}
