3331b4826e336475851a0218fe20fcf09aeaacfb
HafeezOJ <ho.adepoju@gmail.com>
2022-10-15 20:20:54 +0000

    first attempt to print all combination of 2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: prints all possible different combinations of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num, num1;
+
+	for (num = 48; num < 58; num++)
+	{
+		for (num1 = num + 1; num1 < 58; num1++)
+		{
+			putchar(num);
+			putchar(num1);
+			puttchar(',');
+			putchar(' ');
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
