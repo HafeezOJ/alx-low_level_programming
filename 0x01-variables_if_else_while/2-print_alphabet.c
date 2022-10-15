91f760e2ce0113b5ce07a6a4530c37d8308bb0d6
HafeezOJ <ho.adepoju@gmail.com>
2022-10-15 14:33:20 +0000

    C program to write a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: Prints lower alphabets from a - z
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = "a"; alphabet <= "z"; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
