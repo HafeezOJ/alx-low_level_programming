b0f306ae0889d614dc87dd5f01ea7d179719b6a3
HafeezOJ <ho.adepoju@gmail.com>
2022-10-18 23:43:04 +0000

    syntax error fixed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,22 +12,23 @@
 
 void print_to_98(int n)
 {
+	int i;
 	if (n < 98)
 	{
-		for (; n <= 98, n++)
+		for (i = n; i <= 98, i++)
 		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
 	}
 	else
 	{
-		for (; n >= 98; n--)
+		for (i = 0; i >= 98; i--)
 		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
