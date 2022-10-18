751158c149c8c4bb8b177d5f8496106ed197196b
HafeezOJ <ho.adepoju@gmail.com>
2022-10-18 23:16:16 +0000

    add 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * add - print absolute value
+ *
+ * @a: input number as an int
+ * @b: input number as an int
+ *
+ * Description: sums @a and @b
+ *
+ * Return: returns sum of @a and @b as int
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
