63663bd3367e68ddbe12de71c6b4b35b07567383
HafeezOJ <ho.adepoju@gmail.com>
2022-10-19 00:20:28 +0000

    use printf

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,51 +1,27 @@
-#include "main.h"
-#include <stdlib.h>
-/**
- * print_to_98 - prints up to or down to 98 from n
- *
- * @n: input number as an int
- *
- * Description: prints from n to 98
- *
- * Return: void
- */
+#include <stdio.h>
 
+/**
+ * print_to_98 - Prints numbers from n to 98.
+ *
+ * @n: input number.
+ *
+ * Return: no return.
+ */
 void print_to_98(int n)
 {
-	int i;
-
-	if (n < 98)
+	if (n > 98)
 	{
-		for (i = n; i <= 97; i++)
+		for (; n > 98; n--)
 		{
-			if(i < 0)
-			{
-				_putchar('-');
-			}
-			_putchar((abs(i) / 10) + '0');
-			_putchar((abs(i) % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar((98 / 10) + '0');
-			_putchar((98 % 10) + '0');
+			printf("%d, ", n);
 		}
 	}
-	else if (n == 98)
+	else if (n < 98)
 	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else
-	{
-		for (i = 0; i >= 97; i--)
+		for (; n < 98; n++)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar((98 / 10) + '0');
-			_putchar((98 % 10) + '0');
+			printf("%d, ", n);
 		}
 	}
-
+	printf("%d\n", n);
 }
