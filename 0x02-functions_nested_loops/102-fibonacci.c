9cefd466dd3dbf9f00b289149a0299c957864c7b
HafeezOJ <ho.adepoju@gmail.com>
2022-10-19 19:10:28 +0000

    first 50 numbers of fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: void
+ */
+int main(void)
+{
+	int first_num, second_num, next_num, count;
+	long int sum;
+
+	first_num = 1;
+	second_num = 2;
+	count = 0;
+
+	printf("%d", first_num);
+	putchar(',');
+	putchar(' ');
+	printf("%d", second_num);
+	putchar(',');
+	putchar(' ');
+	count += 2;
+
+	while (count <= 50)
+	{
+		next_num = first_num + second_num;
+		printf("%d", next_num);
+		putchar(',');
+		putchar(' ');
+		first_num = second_num;
+		second_num = next_num;
+		counter++;
+	}
+
+	printf("\n");
+	return (0);
+}
