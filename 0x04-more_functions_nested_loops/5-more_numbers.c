156860d540efb5d2782a86c1359163396a85965d
HafeezOJ <ho.adepoju@gmail.com>
2022-10-20 19:50:03 +0000

    print 0 - 14 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,15 +14,11 @@ void more_numbers(void)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			if (num > 9)
+			if (num > 10)
 			{
 				_putchar((num / 10) + '0');
-				_putchar((num % 10) + '0');
 			}
-			else
-			{
-				_putchar(num + '0');
-			}
+			_putchar((num % 10) + '0');
 		}
 
 	}
