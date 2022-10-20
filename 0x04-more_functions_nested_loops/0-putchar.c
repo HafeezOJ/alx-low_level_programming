198710e18a8c07ddec55de1a466435b3f8b5168e
HafeezOJ <ho.adepoju@gmail.com>
2022-10-20 18:52:04 +0000

    include 0-putchar

---
+++ 0x04-more_functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point.
+ *
+ * Description: Prints _putchar using custom header
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char text[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+	int size = sizeof(text);
+	int counter;
+
+	for (counter = 0; counter < size ; counter++)
+	{
+		_putchar(text[counter]);
+	}
+	_putchar('\n');
+	return (0);
+}
