22708bbafbc87460bf1407b4bf706d88f60f6cc3
HafeezOJ <ho.adepoju@gmail.com>
2022-10-20 19:03:55 +0000

    multiply 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - print absolute value
+ *
+ * @a: input number as an int
+ * @b: input number as an int
+ *
+ * Description: sums @a and @b
+ *
+ * Return: returns product of @a and @b as int
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
