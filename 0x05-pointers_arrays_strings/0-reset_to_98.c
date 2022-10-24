12ea05024655031f11ba1803b111d3d38d0a0d36
HafeezOJ <ho.adepoju@gmail.com>
2022-10-24 16:03:01 +0000

    a function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * reset_to_98 - reset a value to 98 using pointer
+ *
+ * @n: pointer to n
+ *
+ * Description: use pointer to change variable value 
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+# ALX C - Pointers, arrays and strings
+
+This is the directory for my ALX C - Pointers, arrays and strings project files
+
