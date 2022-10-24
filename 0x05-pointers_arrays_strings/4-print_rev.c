1e3c6f3facd08437b81aa4132e93997c17772649
HafeezOJ <ho.adepoju@gmail.com>
2022-10-24 16:44:00 +0000

    loop down to 0 index to print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - writes to stdout
+ *
+ * @s: pointer to string s
+ *
+ * Description: print to standard output
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int l =  strlen(s);
+	int i;
+
+	for (i = (l - 1); i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
