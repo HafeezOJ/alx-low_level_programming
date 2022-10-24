1a6b4d44ea75ba7b730b1cf30085f8296c1d9179
HafeezOJ <ho.adepoju@gmail.com>
2022-10-24 16:35:02 +0000

    print to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - writes to stdout
+ *
+ * @str: pointer to string str
+ *
+ * Description: print to standard output
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	puts(*str);
+}
