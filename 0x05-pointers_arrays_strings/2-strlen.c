4ea058b47afc0551d8acea6142b2843101c1097d
HafeezOJ <ho.adepoju@gmail.com>
2022-10-24 16:24:17 +0000

    return length of string pointed to by s

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - reset a value to 98 using pointer
+ *
+ * @s: pointer to char
+ *
+ * Description: return length of a string
+ *
+ * Return: int value
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
