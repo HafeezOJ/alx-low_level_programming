ead9c99ef2c8eb2e6a26b24184ab593753dff345
HafeezOJ <ho.adepoju@gmail.com>
2022-10-25 19:30:52 +0000

    remove m

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,15 +12,14 @@
 
 void puts_half(char *str)
 {
-	int length = strlen(str), i, m, n;
+	int length = strlen(str), i, n;
 
 	n = length / 2;
-	m = n;
 
 	if (length % 2 != 0)
 	{
 		n = (length - 1) / 2;
-		m = n -	1;
+		/*m = n - 1; */
 	}
 
 	for (i = n; i < length; i++)
