0944edc51fd9096ad97b880b7814b32f86831f2f
HafeezOJ <ho.adepoju@gmail.com>
2022-10-25 20:17:29 +0000

    convert string to int with atoi function

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _atoi - convert string to in
+ *
+ * @s: pointer to string s
+ *
+ * Description: convert string to int
+ *
+ * Return: int
+ */
+
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
