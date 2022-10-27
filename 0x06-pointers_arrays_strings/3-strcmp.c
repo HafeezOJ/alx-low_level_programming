16fe18bf4caa86cbe0329f89a352460880bac26a
HafeezOJ <ho.adepoju@gmail.com>
2022-10-27 19:37:10 +0000

    string comparison

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: pointer to main string
+ * @s2: pointer to comparison string
+ *
+ * Description: compares s1 to s2; return 1 if more, 0 if equal, else -1
+ *
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
