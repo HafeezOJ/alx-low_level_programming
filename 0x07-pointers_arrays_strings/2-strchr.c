a78a5f36c071322e4d6600c0bff714a7151f65a7
HafeezOJ <ho.adepoju@gmail.com>
2022-11-01 19:10:59 +0000

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include <string.h>
+/**
+ * _strchr - locates a char c in char string s
+ *
+ * @s: char string to be searched from
+ * @c: char string to be searched
+ *
+ * Description: searches for
+ *
+ * Return: pointer to char
+ */
+
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
