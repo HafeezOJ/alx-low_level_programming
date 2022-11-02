07627cff8c7f7e528840cd1a34747b5051fe9487
HafeezOJ <ho.adepoju@gmail.com>
2022-11-02 20:23:29 +0000

    diagonal sum of square matrix

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,38 @@
+#include <string.h>
+/**
+ * print_diagsums - print diagonal sums of a square matrix
+ *
+ * @a: multidimensional array of int
+ * @size: size of the array a
+ *
+ * Description: diagonal sums of a square matrix
+ *
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, j, k, m;
+	long int diag1_sum, diag2_sum;
+
+	diag1_sum = 0;
+	diag2_sum = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (j == i)
+			{
+				diag1_sum += *(a + i * size + j);
+				break;
+			}
+		}
+	}
+
+	for (k = 0; k < size; k++)
+	{
+		m = size - k - 1;
+		diag2_sum += *(a + k * size + m);
+	}
+}
