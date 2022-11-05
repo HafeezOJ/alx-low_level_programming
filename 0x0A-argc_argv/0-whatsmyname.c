eb13e9ac759143504ad9ba0566d3b87d716b145a
HafeezOJ <ho.adepoju@gmail.com>
2022-11-05 10:39:33 +0000

    print name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program execution starts here
+ *
+ * Description: print name of the program
+ *
+ * Return: int
+ */
+
+int main(void)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,4 @@
+# C - argc, argv
+
+Folder for my ALX C - argc, argv project files
+
