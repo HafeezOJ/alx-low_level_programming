723de9000e1cf989a39b6d5461a458446f4fb8f7
HafeezOJ <ho.adepoju@gmail.com>
2022-11-05 14:29:11 +0000

    number of coins in cents

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program execution starts here
+ *
+ *@argc: number of arguments
+ *@argv: values of arguments
+ *
+ * Description: number of coins in a sum of money
+ *
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+
+	if (argc - 1 != 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		if (cents >= 10)
+			cents -= 10;
+		if (cents >= 5)
+			cents -= 5;
+		if (cents >= 2)
+			cents -= 2;
+		if (cents >= 1)
+			cents -= 1;
+		coins += 1;
+	}
+
+	printf("%d\n", coins);
+	return (0);
+}
