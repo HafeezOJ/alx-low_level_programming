521652c78aaee02ade323c250d77e08e5a34aa94
HafeezOJ <ho.adepoju@gmail.com>
2022-11-10 22:45:08 +0000

    free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - clear memomry held by user
+ * 
+ * @grid: multidimensional arra
+ * @height: height of the grid.
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	if (grid != NULL && height != 0)
+	{
+		for (; height >= 0; height--)
+			free(grid[height]);
+		free(grid);
+	}
+}
