b432df4e9633ba2be993af1e3606c620aa871577
HafeezOJ <ho.adepoju@gmail.com>
2022-11-14 23:24:36 +0000

    typedef doc

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,6 +9,12 @@
  *
  */
 
+/**
+ * type dog
+ */
+
+typedef struct dog dog_t;
+
 struct dog
 {
 	char *name;
@@ -18,5 +24,6 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 
 #endif
